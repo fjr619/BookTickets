4a1841319cb61ab42b52e7d5a7f978945fce2636
Franky Wijanarko <frank.jr.619@gmail.com>
2023-10-18 08:05:45 -0700

    home screen: search bar

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:booktickets/util/app_style.dart';
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
@@ -33,7 +34,25 @@ class HomeScreen extends StatelessWidget {
                       child: Image.asset("assets/images/img_1.png", height: 50, width: 50, fit: BoxFit.fill,),
                     )
                   ],
+                ),
+                const Gap(25),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xFFF4F6FD)
+                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                  child: Row(
+                  children: [
+                    const Icon(FluentIcons.search_24_regular, color: Color(0xFFBFC205),),
+                    Text(
+                      "Search",
+                      style: Styles.headlineTextStyle4,
+                    )
+                  ],
+                ),
                 )
+                
               ],
             ),
           )
